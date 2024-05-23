34bfb827e4201ded92e54c69dccef5480d263b9c
evgeniymoiseev <evgeniy.moiseev.1985@gmail.com>
2024-05-23 22:00:57 +0300

    test

--- src/main/kotlin/com/sands/treasure/Application.kt
+++ src/main/kotlin/com/sands/treasure/Application.kt
@@ -1,14 +1,13 @@
 package com.sands.treasure
 
 import com.sands.treasure.plugins.configureDatabases
-import com.sands.treasure.plugins.configureRouting
 import com.sands.treasure.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    //embeddedServer(Netty, port = 8100, host = "192.168.0.102", module = Application::module)
+    //embeddedServer(Netty, port = 8100, host = "192.168.0.100", module = Application::module).start(wait = true)
     embeddedServer(Netty, port = System.getenv("PORT")?.toInt() ?: 8080, module = Application::module)
         .start(wait = true)
 }
@@ -16,5 +15,4 @@ fun main() {
 fun Application.module() {
     configureSerialization()
     configureDatabases()
-    configureRouting()
 }
--- src/main/kotlin/com/sands/treasure/plugins/Databases.kt
+++ src/main/kotlin/com/sands/treasure/plugins/Databases.kt
@@ -6,29 +6,27 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.delay
 
 fun Application.configureDatabases() {
 
-    val connectionString =
+    /*val connectionString =
         "mongodb+srv://yevhenmoiseiev:mais1208@cluster0.rilg0du.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     val client = MongoClient.create(connectionString = connectionString)
     val database = client.getDatabase(databaseName = "TreasurePlayers")
-    val playerService = PlayerService(database)
+    val playerService = PlayerService(database)*/
 
     routing {
-        // Create user
-        post("/players") {
+       /* post("/players") {
             val player = call.receive<Player>()
             val id = playerService.create(player)
             println("Save player: $player")
             call.respond(HttpStatusCode.Created)
         }
-
         get("/players"){
             val players = playerService.leaderboardPlayers()
             call.respond(HttpStatusCode.OK, players)
         }
-
         get("/players/{id}") {
             val id = call.parameters["id"] ?: ""
             val player = playerService.player(id)
@@ -39,12 +37,62 @@ fun Application.configureDatabases() {
                 call.respond(HttpStatusCode.NotFound)
             }
         }
-
         put("/players/{id}") {
             val id = call.parameters["id"] ?: ""
             val player = call.receive<Player>()
             playerService.update(id, player)
             call.respond(HttpStatusCode.OK)
+        }*/
+
+        get("/link"){
+            println("=========================REQUEST===============================")
+            val a = call.request.queryParameters["a"]
+            val b = call.request.queryParameters["b"]
+            call.respondText("https://${a}_and_${b}")
         }
+
+        get("/link1"){
+            delay(1000)
+            println("=========================REQUEST===============================")
+            val a = call.request.queryParameters["a"]
+            val b = call.request.queryParameters["b"]
+            call.respondText("https://${a}_and_${b}")
+        }
+        get("/link2"){
+            delay(2000)
+            println("=========================REQUEST===============================")
+            val a = call.request.queryParameters["a"]
+            val b = call.request.queryParameters["b"]
+            call.respondText("https://${a}_and_${b}")
+        }
+        get("/link3"){
+            delay(3000)
+            println("=========================REQUEST===============================")
+            val a = call.request.queryParameters["a"]
+            val b = call.request.queryParameters["b"]
+            call.respondText("https://${a}_and_${b}")
+        }
+        get("/link4"){
+            delay(4000)
+            println("=========================REQUEST===============================")
+            val a = call.request.queryParameters["a"]
+            val b = call.request.queryParameters["b"]
+            call.respondText("https://${a}_and_${b}")
+        }
+        get("/link5"){
+            delay(5000)
+            println("=========================REQUEST===============================")
+            val a = call.request.queryParameters["a"]
+            val b = call.request.queryParameters["b"]
+            call.respondText("https://${a}_and_${b}")
+        }
+        get("/link6"){
+            delay(6000)
+            println("=========================REQUEST===============================")
+            val a = call.request.queryParameters["a"]
+            val b = call.request.queryParameters["b"]
+            call.respondText("https://${a}_and_${b}")
+        }
+
     }
 }
